bedc2644b96b3e8f04b6c1f03fa33523c2fca795
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-01-29 17:16:51 +0530

    create a route to get a particular model info

--- server/src/controllers/model.controller.ts
+++ server/src/controllers/model.controller.ts
@@ -12,3 +12,30 @@ export const listModels = async (req: Request, res: Response) => {
     return;
   }
 };
+
+export const modelInfo = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.params;
+    const response = await fetch(`${OLLAMA_API_URL}/show`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: name }),
+    });
+    const data = await response.json();
+    if (data.error) {
+      res.status(404).json({ success: false, message: "Model not found" });
+      return;
+    }
+    res
+      .status(200)
+      .json({ success: true, message: `Model info for ${name}`, data });
+    return;
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Error fetching model info" });
+    return;
+  }
+};
--- server/src/routes/models.route.ts
+++ server/src/routes/models.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { listModels } from "../controllers/model.controller";
+import { listModels, modelInfo } from "../controllers/model.controller";
 
 const router = Router();
 
 router.route("/listModels").get(listModels);
+router.route("/modelInfo/:name").get(modelInfo);
 
 export default router;
