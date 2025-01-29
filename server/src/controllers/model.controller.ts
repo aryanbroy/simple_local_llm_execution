2be5bf1dc6711d847886026884f01e5f5f1fd8a0
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-01-29 17:40:59 +0530

    create route to generate response from llm

--- server/src/controllers/model.controller.ts
+++ server/src/controllers/model.controller.ts
@@ -25,7 +25,11 @@ export const modelInfo = async (req: Request, res: Response) => {
     });
     const data = await response.json();
     if (data.error) {
-      res.status(404).json({ success: false, message: "Model not found" });
+      res.status(404).json({
+        success: false,
+        message: "Model not found",
+        error: data.error,
+      });
       return;
     }
     res
@@ -37,5 +41,34 @@ export const modelInfo = async (req: Request, res: Response) => {
       .status(500)
       .json({ success: false, message: "Error fetching model info" });
     return;
+  }
+};
+
+export const generateResponse = async (req: Request, res: Response) => {
+  try {
+    const { model, prompt } = req.body;
+
+    if (!model || !prompt) {
+      res
+        .status(400)
+        .json({ success: false, message: "Model or prompt is missing" });
+      return;
+    }
+    const response = await fetch(`${OLLAMA_API_URL}/generate`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ model, prompt, stream: false }),
+    });
+    const data = await response.json();
+    res
+      .status(200)
+      .json({ success: true, message: "Response generated", data });
+    return;
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Error generating response" });
   }
 };
--- server/src/routes/models.route.ts
+++ server/src/routes/models.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { listModels, modelInfo } from "../controllers/model.controller";
+import {
+  generateResponse,
+  listModels,
+  modelInfo,
+} from "../controllers/model.controller";
 
 const router = Router();
 
 router.route("/listModels").get(listModels);
 router.route("/modelInfo/:name").get(modelInfo);
+router.route("/generateResponse").post(generateResponse);
 
 export default router;
